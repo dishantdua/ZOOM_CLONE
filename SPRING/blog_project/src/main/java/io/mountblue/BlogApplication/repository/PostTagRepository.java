838a4ca9d8d9ca6e406659feb509f611a6e45f2e
dishantdua <87745290+dishantdua@users.noreply.github.com>
2024-04-10 11:35:48 +0530

    Delete SPRING/blog_project directory

--- SPRING/blog_project/src/main/java/io/mountblue/BlogApplication/repository/PostTagRepository.java
+++
@@ -1,7 +0,0 @@
-package io.mountblue.BlogApplication.repository;
-
-import io.mountblue.BlogApplication.entity.PostTag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostTagRepository extends JpaRepository<PostTag, Long> {
-}
